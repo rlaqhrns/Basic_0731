f58d11964d4086bfeb4da81b1f4a3c90d678f913
rlaqhrns93@naver.com <Green_Computer@DESKTOP-912HOCH>
2020-07-31 18:39:56 +0900

    h1

---
+++ src/poly/ImplAttatchable.java
@@ -0,0 +1,23 @@
+package poly;
+
+public class ImplAttatchable implements IAttachable {
+
+	@Override
+	public void driving() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int sum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float sum(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
